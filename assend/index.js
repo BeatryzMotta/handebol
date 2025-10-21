ba89060dea7981596504da6349b179e72e0d9271
Beatryz Oliveira Motta <bya06motta@gmail.com>
2025-10-21 10:26:31 -0300

    Add files via upload

---
+++ assend/index.js
@@ -0,0 +1,11 @@
+ // Efeito de aparecer ao rolar
+    const sections = document.querySelectorAll("section");
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => observer.observe(section));
